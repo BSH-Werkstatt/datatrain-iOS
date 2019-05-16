1ec1112dcb7e8741674d14d2d306595e78853b43
Susanne Winkler <s.winkler@tum.de>
2019-05-16 19:43:50 +0200

    Demo App for UE Meeting 3
    add demo Buttons & view
    add feature crumbs

--- BSH/BSH/ViewController.swift
+++ BSH/BSH/ViewController.swift
@@ -5,16 +5,46 @@
 //  Created by Emil Oldenburg on 07.05.19.
 //  Copyright © 2019 Tum LS1. All rights reserved.
 //
-
+// MARK: Imports
 import UIKit
 
+// MARK: - ViewController
 class ViewController: UIViewController {
-
+    // MARK: IBOutlets
+    @IBOutlet private weak var outputLabel: UILabel!
+    
+    // MARK: IBActions
+    @IBAction private func uploadImage() {
+        if let pictureAmountString = outputLabel.text,
+            let pictureAmount = Int(pictureAmountString) {
+            outputLabel.text = String(pictureAmount + 1)
+            // Feature Crumb Name: "<Upload Picture>"
+            //CUU.seed(name: "Step 1: Select Item")
+        }
+    }
+    
+    
+    @IBAction private func addLabel() {
+        print("Label added")
+        //not implemented labeling yet
+        // Feature Crumb Name: "<adding Label>"
+        // CUU.seed(name: "Step 1: Select Item")
+    }
+    
+    
+    
+    @IBAction private func leaderboardPressed() {
+        print("Leaderboard Button pressed")
+        // Feature Crumb Name: "<Go to Leaderboard>"
+        // CUU.seed(name: "Step 1: Select Item")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
