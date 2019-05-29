5ad6b3e6ce20772ddf92fbafe52a89a8c42e643a
Susanne Winkler <s.winkler@tum.de>
2019-05-29 17:23:55 +0200

    Create Storyboards
    add Campaign Storyboards
    add Leaderboard Storyboards
    create ViewControllers
    refractor Campaign Storyboards

---
+++ BSH/BSH/Controllers/UploadDataViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UploadDataViewController.swift
+//  BSH
+//
+//  Created by Susanne Winkler on 29.05.19.
+//  Copyright © 2019 Tum LS1. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CUU
+
+// MARK: - UploadDataViewController
+class UploadDataViewController: CUUViewController {
+    
+    // MARK: Overriden Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
