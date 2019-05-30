227dc8d6a317dc5c3e53e577e6b8517562e93a03
Lei, Taylor <taylor.lei@tum.de>
2019-05-30 10:16:25 +0200

    Sample usage of SwaggerClient

--- BSH/BSH/Controllers/CampaignViewController.swift
+++ BSH/BSH/Controllers/CampaignViewController.swift
@@ -10,18 +10,39 @@
 import Foundation
 import UIKit
 import CUU
+import SwaggerClient
 
 // MARK: - CampaignViewTableControll
 class CampaignViewController: CUUTableViewController {
     // MARK: IBOUtlet
     @IBOutlet private weak var campaignTable: UITableView!
     
-    //!! Need to add Table Mockdata
+    private var campaigns: [Campaign] = []
+    private var activeCampaign: Campaign?
     
     // MARK: Overriden Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        loadCampaigns()
     }
     
+    /// Gets the currently active campaign, which was selected in the CampaignView
+    public func getActiveCampaign() -> Campaign? {
+        return activeCampaign
+    }
+    
+    /// Loads all existing campaigns from the database and stores them in the campaigns property
+    private func loadCampaigns() {
+        // we don't take the user into account yet
+        DefaultAPI.getAllCampaigns(completion: { campaigns, error in
+            guard let campaigns = campaigns else {
+                print(error ?? "Unspecified Error")
+                return
+            }
+            
+            self.campaigns = campaigns
+        })
+    }
 }
