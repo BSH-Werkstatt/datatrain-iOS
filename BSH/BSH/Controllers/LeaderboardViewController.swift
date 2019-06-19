3aa08c47c186ebf31d319f52f38f7fa5486d68c1
Emil Oldenburg <emil.oldenburg@tum.de>
2019-06-20 01:09:54 +0200

    why....

--- BSH/BSH/Controllers/LeaderboardViewController.swift
+++ BSH/BSH/Controllers/LeaderboardViewController.swift
@@ -9,10 +9,18 @@
 // MARK: Imports
 import Foundation
 import UIKit
+import SwaggerClient
 import CUU
+
+class LeaderboardCellController: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+}
 
 // MARK: - LeaderboardViewController
 class LeaderboardViewController: CUUTableViewController {
+
+    var leaderboard: Leaderboard
     
     // MARK: IBOutlets
     
@@ -21,7 +29,40 @@ class LeaderboardViewController: CUUTableViewController {
     // MARK: Overriden Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getLeaderboard()
+    }
+
+    func getLeaderboard(id:String = "5d0a6fe5a9edbb9d5cc29e11"){
+        DefaultAPI.getLeaderboard(campaignId: id, completion: {
+            leaderboard, error in
+            guard let leaderboard = leaderboard else {
+                print(error ?? "whatever")
+                return
+            }
+            self.leaderboard = leaderboard
+        })
+    }
+
+    // MARK: Table View Data Source methods
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return leaderboard.scores.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "leaderboardCell", for: indexPath) as! LeaderboardCellController
+
+        // get the current cell from campaigns sorted by ids
+        let score = leaderboard.scores.sorted(by: { $0.score < $1.score })[indexPath.row]
+
+        let formater = NumberFormatter()
+        formater.numberStyle = .ordinal
+
+        cell.titleLabel?.text = formater.string(from: NSNumber(value: indexPath.row))
+        cell.descriptionLabel?.text = score.userId
+
+
+        return cell
     }
 }
 
