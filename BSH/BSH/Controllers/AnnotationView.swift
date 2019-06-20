75912e9b73c238bcda88801c192fc2cd9cfcf85e
Baris Sen <baris.sen@tum.de>
2019-06-20 02:33:33 +0200

    Create AnnotationView class

---
+++ BSH/BSH/Controllers/AnnotationView.swift
@@ -0,0 +1,21 @@
+//
+//  AnnotationView.swift
+//  BSH
+//
+//  Created by Baris Sen on 6/20/19.
+//  Copyright © 2019 Tum LS1. All rights reserved.
+//
+
+import UIKit
+
+class AnnotationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
