bea9b4aa642f628f1645fefb27d659d764f83243
Susanne Winkler <s.winkler@tum.de>
2019-07-08 18:34:20 +0200

    images profile

--- BSH/BSH/Controllers/ProfileNewController.swift
+++ BSH/BSH/Controllers/ProfileNewController.swift
@@ -1,36 +1,38 @@
 //
-//  ProfileController.swift
+//  ProfileNewController.swift
 //  BSH
 //
-//  Created by Emil Oldenburg on 20.06.19.
+//  Created by Susanne Winkler on 08.07.19.
 //  Copyright © 2019 Tum LS1. All rights reserved.
 //
 
-// MARK: Imports
 import Foundation
+
 import UIKit
 import CUU
 import SwaggerClient
 
 // MARK: - LeaderboardViewController
-class ProfileViewController: CUUTableViewController {
-
+class ProfileNewController: CUUViewController {
+    
     var user: User?
     // MARK: IBOutlets
-
-
     @IBOutlet weak var emailLabel: UILabel!
-    @IBOutlet weak var idLabel: UILabel!
-    @IBOutlet var profileTableView: UITableView!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var buildVersionLabel: UILabel!
     
-    // MARK: Overriden Methods
+// MARK: Overriden Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureRefreshControl ()
+    //configureRefreshControl () not possible in UIView
         getProfile()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.setToolbarHidden(true, animated: false)
+    }
+    
     func getProfile() {
         guard let email = UserDefaults.standard.string(forKey: "user-email") else {
             return
@@ -43,35 +45,42 @@ class ProfileViewController: CUUTableViewController {
             }
             self.user = user
             self.emailLabel.text = user.email
-            self.idLabel.text = user.name
+            self.nameLabel.text = user.name
             self.buildVersionLabel.text = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "No build number found"
         })
     }
-
+    
     //refreshing controls
-
-    func configureRefreshControl () {
+    
+    /*func configureRefreshControl () {
         // Add the refresh control to your UIScrollView object.
         profileTableView.refreshControl = UIRefreshControl()
         profileTableView.refreshControl?.addTarget(self, action:
             #selector(refreshData(sender:)), for: .valueChanged)
     }
-
+    
     @objc func refreshData(sender: UIRefreshControl) {
         getProfile()
         DispatchQueue.main.async {
             self.profileTableView.refreshControl?.endRefreshing()
         }
-    }
-
+     
+     }
+ */
+    
     @IBAction func dismissProfile(_ sender: Any) {
         dismiss(animated: true, completion: nil)
-
+        
     }
-
+    
     @IBAction func logout(_ sender: Any) {
         UserDefaults.standard.set(false, forKey: "loggedIn")
         Switcher.updateRootVC()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        navigationController?.setToolbarHidden(false, animated: false)
     }
     
 }
