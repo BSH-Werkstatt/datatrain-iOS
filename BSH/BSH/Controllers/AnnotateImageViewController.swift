3549b126d54e604ece1c59dc550ab03fed7d4a36
Baris Sen <baris.sen@tum.de>
2019-07-18 19:20:52 +0200

    Fix long press magnifier bug

--- BSH/BSH/Controllers/AnnotateImageViewController.swift
+++ BSH/BSH/Controllers/AnnotateImageViewController.swift
@@ -387,14 +387,23 @@ class AnnotateImageViewController: CUUViewController, UITextFieldDelegate {
     }
     
     @IBAction func handleLongPressGestures(longPressGestureRecognizer: UILongPressGestureRecognizer) {
-        if longPressGestureRecognizer.state == .began {
+        switch longPressGestureRecognizer.state {
+        case .began:
             if magnifyView == nil {
                 magnifyView = MagnifyView.init(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
                 magnifyView!.viewToMagnify = self.view.superview
                 self.view.superview?.addSubview(magnifyView!)
                 magnifyView!.setTouchPoint(pt: longPressGestureRecognizer.location(in: self.view.superview))
             }
+        case .ended:
+            if magnifyView != nil {
+                magnifyView!.removeFromSuperview()
+                magnifyView = nil
+            }
+        default:
+            return
         }
+        
     }
     
     @IBAction func handleRotationGestures(rotationGestureRecognizer: UIRotationGestureRecognizer) {
